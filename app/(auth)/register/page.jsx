601d4a21f7a79ce628eab47c46b40b8061c3b6cd
Ajinkya Bhandare <112012663+ajinkyavbhandare@users.noreply.github.com>
2025-09-23 21:04:10 +0530

    Update reg red url

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -11,7 +11,7 @@ export default function RegisterPage() {
     await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `http://localhost:3000/callback`,
+        redirectTo: `https://palacevr.vercel.app/callback`,
       },
     });
   };
